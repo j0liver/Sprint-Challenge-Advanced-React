0d91c8def1777c00ff0a35bb755e7979f6e11002
Joe O <j0liverko@gmail.com>
2019-11-01 12:56:50 -0500

    Added two test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import * as rtl from '@testing-library/react';
+
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+
+test('App renders without crashing', () => {
+  rtl.render(<App />);
+})
+
+test('app class has data'), () => {
+  const wrapper = rtl.render(<App />)
+  expect(wrapper.getByText(/Alex Morgan/))
+}
